95fd4ab303c482781506026cfdb3ee9992141f3e
Kashif <sayedkaskai22@gmail.com>
2024-07-09 14:13:09 +0200

    first project from mama

---
+++ randomExercice_pt4.js
@@ -0,0 +1,23 @@
+// 1s and 0
+
+function textToNumber(str){
+    const words = str.split(' ').filter(word => word.toLowerCase() === "zero" || word.toLowerCase() === "one");
+    
+    console.log(words);
+
+    let binaryString = words.map(()=>{
+        return word.toLowerCase()=== "one" ? "1" : "0"
+    }).join(" ");
+
+    const excessLength = binaryString.length % 8;
+
+    if(excessLength!==0){
+        binaryString = binaryString.slice(0,0);
+    }
+
+    return binaryString;
+
+    
+}
+
+console.log(textToNumber("Zero one zero ONE zero one"));
